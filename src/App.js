abf3d47f762352507342f42ce780a8a844fa54aa
Marian Boda <marianboda@gmail.com>
2019-08-21 01:32:29 +0200

    Added reading files to frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function FileView({ data }) {
+    return (
+        <div>
+            {data.id} {data.path}
+        </div>
+    )
 }
 
-export default App;
+function scan(path) {
+    console.log('will be scanning', path)
+    fetch('/scan', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ path }),
+    })
+}
+
+function App() {
+    const [files, setFiles] = useState([])
+    const [path, setPath] = useState('')
+
+    const handleClick = () => {
+        fetch('/files').then(r => r.json())
+        .then(r => setFiles(r))
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                Photor III
+            </header>
+            <div>
+                <input type="text" value={path} onChange={e => setPath(e.target.value)}/>
+                <button type="button" onClick={() => scan(path)}>SCAN</button>
+                <button type="button" onClick={handleClick}>OK</button>
+            </div>
+            <div>
+                {files.map(i => <FileView data={i} />)}
+            </div>
+        </div>
+    )
+}
+
+export default App
