610ad55ec394bbb39976ae5f0a3f5a57be6b76b3
Marian Boda <marianboda@gmail.com>
2020-08-19 00:44:55 +0200

    Clean up

--- src/ListScreen.js
+++ src/ListScreen.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { sort, prop, descend } from 'ramda';
-import { DirView } from './components/DirView';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { sort, prop, descend } from 'ramda'
+import { DirView } from './components/DirView'
 
 export const ListScreen = () => {
-    const dispatch = useDispatch();
-    const getDirs = () => dispatch({ type: 'GET_DIRS' });
+    const dispatch = useDispatch()
+    const getDirs = () => dispatch({ type: 'GET_DIRS' })
 
-    const dirs = useSelector(state => state.dirs);
-    const scanningPaths = useSelector(state => state.scanningPaths);
+    const dirs = useSelector(state => state.dirs)
+    // const scanningPaths = useSelector(state => state.scanningPaths);
 
     return (
         <div className="Scan-screen">
@@ -18,8 +18,10 @@ export const ListScreen = () => {
                 </button>
             </div>
             <div className="Files-content">
-                <div>{dirs && sort(descend(prop('filesCount')), dirs).map(i => <DirView data={i} />)}</div>
+                <div>
+                    {dirs && sort(descend(prop('filesCount')), dirs).map(i => <DirView data={i} />)}
+                </div>
             </div>
         </div>
-    );
-};
+    )
+}
--- src/middleware.js
+++ src/middleware.js
@@ -2,44 +2,48 @@ async function post(path, body = {}) {
     return fetch(path, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
-    }).then(r => r.json());
+        body: JSON.stringify(body),
+    }).then(r => r.json())
 }
 
 async function get(path) {
-    return fetch(path).then(r => r.json());
+    return fetch(path)
+        .then(r => r.json())
+        .catch(e => console.error(e))
 }
 
 export const requester = store => next => action => {
     switch (action.type) {
         case 'GET_FILES':
-            get('/files').then(r => store.dispatch({ type: 'SET_FILES', payload: r }));
-            get('/stats').then(r => store.dispatch({ type: 'SET_STATS', payload: r }));
-            return next(action);
+            get('/files').then(r => store.dispatch({ type: 'SET_FILES', payload: r }))
+            get('/stats').then(r => store.dispatch({ type: 'SET_STATS', payload: r }))
+            return next(action)
         case 'GET_DIRS':
-            get('/dirs').then(r => store.dispatch({ type: 'SET_DIRS', payload: r }));
-            return next(action);
+            get('/dirs')
+                .then(r => store.dispatch({ type: 'SET_DIRS', payload: r }))
+                .catch(e => console.error(e))
+            return next(action)
         case 'GET_SCANNING_PATHS':
-            get('/scanning-paths').then(r => store.dispatch({ type: 'SET_SCANNING_PATHS', payload: r }));
-            return next(action);
+            get('/scanning-paths').then(r =>
+                store.dispatch({ type: 'SET_SCANNING_PATHS', payload: r }),
+            )
+            return next(action)
         case 'HASH_FILE':
-            post('/hashFile').then(() => store.dispatch({ type: 'GET_FILES' }));
-            return next(action);
+            post('/hashFile').then(() => store.dispatch({ type: 'GET_FILES' }))
+            return next(action)
         case 'SAVE_SCANNING_PATH':
-            post('/scanning-path', action.payload).then(() =>
-                store.dispatch({ type: 'GET_FILES' })
-            );
-            return next(action);
+            post('/scanning-path', action.payload).then(() => store.dispatch({ type: 'GET_FILES' }))
+            return next(action)
         case 'SCAN_START':
-            post('/scan-start', action.payload);
-            return next(action);
+            post('/scan-start', action.payload)
+            return next(action)
         case 'SCAN_STOP':
-            post('/scan-stop', action.payload);
-            return next(action);
+            post('/scan-stop', action.payload)
+            return next(action)
         case 'PROCESS_DEEPEST':
-            post('/process-deepest-dir');
-            return next(action);
+            post('/process-deepest-dir')
+            return next(action)
         default:
-            return next(action);
+            return next(action)
     }
-};
+}
